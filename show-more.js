f1287872d10b01af35ae81aa44d832932c335cfe
kvakich <83812612+kv4kich@users.noreply.github.com>
2022-12-22 06:12:46 +0800

    Add files via upload

--- show-more.js
+++ show-more.js
@@ -1,20 +1,24 @@
 const showMore = document.querySelector('.show-more');
-const slideLength = document.querySelectorAll('.swiper-slide').length;
-let items = 6;
+const sliderDropdown = document.querySelector('.section__slider')
 
 showMore.addEventListener('click', () => {
-    items = slideLength;
-    const array = Array.from(document.querySelector('.swiper-wrapper').children);
-    const visibleItems = array.slice(0, items);
 
     if (showMore.textContent == "Скрыть") {
+
         showMore.classList.contains("read-more--text-true") ? showMore.textContent ="Читать далее"
         :showMore.textContent = "Показать всё";
-        visibleItems.forEach(el => el.classList.remove('is-visible'));
+
     } else {
+
         showMore.textContent = "Скрыть";
-        visibleItems.forEach(el => el.classList.add('is-visible'));
-    } 
+    
+    }
+
+    showMore.classList.toggle("show-more--passive");
+    showMore.classList.toggle("show-more--active");
+
+    sliderDropdown.classList.toggle('is-visible'); 
+    
 });
 
 
